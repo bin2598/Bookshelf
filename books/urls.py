57229dfec88b9df356ba6b260739385e3aacef94
Bibin AB <bibinbabyanthony@gmail.com>
2021-02-12 18:04:33 +0530

    'views_created'

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . views import BooklistView,BookdetailView,BookcreateView,BookeditView,BookdeleteView
+
+urlpatterns = [
+    path('', BooklistView.as_view(), name='book-list'),
+    path('detail/<int:pk>', BookdetailView.as_view(), name='book-detail'),
+    path('create', BookcreateView.as_view(), name='book-create'),
+    path('edit/<int:pk>', BookeditView.as_view(), name='book-edit'),
+    path('delete/<int:pk>', BookdeleteView.as_view(), name='book-delete')
+]
--- books/views.py
+++ books/views.py
@@ -1,1 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .models import Book
+from django.urls import reverse_lazy
+
+class BooklistView(ListView):
+    model = Book
+    template_name = 'bookList.html'
+
+
+class BookdetailView(DetailView):
+    model = Book
+    template_name = 'bookDetail.html'
+
+
+class BookcreateView(CreateView):
+    model = Book
+    template_name = 'bookCreate.html'
+    fields = '__all__'
+
+
+class BookeditView(UpdateView):
+    model = Book
+    template_name = 'bookEdit.html'
+    fields = '__all__'
+
+
+class BookdeleteView(DeleteView):
+    model = Book
+    template_name = 'bookDelete.html'
+    fields = '__all__'
+    success_url = reverse_lazy('book-list')
